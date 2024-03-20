ce7ceddf8c53bf5ecbecc8c954d2ba0ddc24d071
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-03-20 12:24:41 -0400

    fixed callback router to use passport as middleware, and added logic in spotifyStragey to create new users or update their access tokens

--- server/config/strategies/spotifyStrategy.js
+++ server/config/strategies/spotifyStrategy.js
@@ -1,3 +1,4 @@
+const { User, Auth } = require('../../models');
 const SpotifyStrategy = require('passport-spotify').Strategy;
 require('dotenv').config();
 
@@ -9,7 +10,43 @@ module.exports = new SpotifyStrategy(
 	},
 	async function (accessToken, refreshToken, expires_in, profile, done) {
 		try {
-			return done(null, profile);
+			let user = await User.findOne({
+				email: profile.emails[0].value,
+			}).populate({
+				path: 'authMethods',
+				match: { authProvider: profile.provider },
+			});
+
+			if (!user) {
+				user = await User.create({
+					username: profile.username,
+					displayName: profile.displayName,
+					email: profile.emails[0].value,
+					profileImage: profile.photos[0].value,
+				});
+
+				let newAuthMethod = await Auth.create({
+					authProvider: profile.provider,
+					profileId: profile.id,
+					encryptedAuthToken: accessToken,
+					encryptedRefreshToken: refreshToken,
+					expirationDate: new Date(Date.now() + expires_in * 1000),
+					profileLink: profile.profileUrl,
+				});
+
+				user.authMethods.push(newAuthMethod);
+			} else {
+				let authMethod = await Auth.findOne({ _id: user.authMethods[0]._id });
+				authMethod.encryptedAuthToken = accessToken;
+				authMethod.encryptedRefreshToken = refreshToken;
+				authMethod.expirationDate = new Date(Date.now() + expires_in * 1000);
+
+				await authMethod.save();
+			}
+
+			await user.save();
+
+			return done(null, user);
 		} catch (error) {
 			console.error(error);
 			return done(error);
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 module.exports = {
 	spotifyCallback: (req, res) => {
+		console.log(req.session.passport);
 		res.send('Spotify Logged in!');
 	},
 };
