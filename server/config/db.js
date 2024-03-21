a5c59d7f07cf623e5012b4c513bc2dcbd8cffad7
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-03-21 16:36:38 -0400

    refactored code in spotifyStrategy file into different functions to improve modularity and readibility

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,36 @@
+/**
+ * MongoDB Connection Initialization.
+ *
+ * This module establishes a connection to the MongoDB database using Mongoose.
+ * It leverages environment variables for configuration to enhance security and flexibility.
+ * Proper error handling mechanisms are employed to ensure reliability and maintainability.
+ */
+
+// External module imports.
 const mongoose = require('mongoose');
 
-const connectDB = () => {
-	return mongoose.connect('mongodb://127.0.0.1:27018/vibestreamDB', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+// Load environment variables from .env file using dotenv.
+require('dotenv').config();
+
+/**
+ * Initiates a connection to the MongoDB database.
+ * Utilizes Mongoose for ORM capabilities and leverages connection string from environment variables.
+ *
+ * @async
+ * @function connectDB
+ * @returns {Promise<void>} Resolves upon successful connection, or rejects with an error if connection fails.
+ */
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_URI, {
+			useNewUrlParser: true, // Deprecation workaround for URL string parsing.
+			useUnifiedTopology: true, // Deprecation workaround for Server Discover and Monitoring engine.
+		});
+		console.log('MongoDB successfully connected.'); // Log on successful connection.
+	} catch (error) {
+		console.error('Database connection failed:', error); // Log and re-throw on connection error for further handling.
+		throw error;
+	}
 };
 
 module.exports = connectDB;
--- server/config/strategies/spotifyStrategy.js
+++ server/config/strategies/spotifyStrategy.js
@@ -10,42 +10,8 @@ module.exports = new SpotifyStrategy(
 	},
 	async function (accessToken, refreshToken, expires_in, profile, done) {
 		try {
-			let user = await User.findOne({
-				email: profile.emails[0].value,
-			}).populate({
-				path: 'authMethods',
-				match: { authProvider: profile.provider },
-			});
-
-			if (!user) {
-				user = await User.create({
-					username: profile.username,
-					displayName: profile.displayName,
-					email: profile.emails[0].value,
-					profileImage: profile.photos[0].value,
-				});
-
-				let newAuthMethod = await Auth.create({
-					authProvider: profile.provider,
-					profileId: profile.id,
-					encryptedAuthToken: accessToken,
-					encryptedRefreshToken: refreshToken,
-					expirationDate: new Date(Date.now() + expires_in * 1000),
-					profileLink: profile.profileUrl,
-				});
-
-				user.authMethods.push(newAuthMethod);
-			} else {
-				let authMethod = await Auth.findOne({ _id: user.authMethods[0]._id });
-				authMethod.encryptedAuthToken = accessToken;
-				authMethod.encryptedRefreshToken = refreshToken;
-				authMethod.expirationDate = new Date(Date.now() + expires_in * 1000);
-
-				await authMethod.save();
-			}
-
-			await user.save();
-
+			let callbackData = { accessToken, refreshToken, expires_in, profile };
+			let user = await authenticateUser(callbackData);
 			return done(null, user);
 		} catch (error) {
 			console.error(error);
@@ -53,3 +19,87 @@ module.exports = new SpotifyStrategy(
 		}
 	}
 );
+
+async function authenticateUser(callbackData) {
+	try {
+		const {
+			profile: { username, emails, displayName, photos, provider },
+		} = callbackData;
+
+		let user = await User.findOne({
+			email: emails[0].value,
+		}).populate({
+			path: 'authMethods',
+		});
+
+		if (!user) {
+			user = await User.create({
+				username: username,
+				displayName: displayName,
+				email: emails[0].value,
+				profileImage: photos[0].value,
+			});
+
+			let newAuthMethod = await createAuthMethod(callbackData);
+
+			user.authMethods.push(newAuthMethod);
+		} else {
+			let authIndex = user.authMethods.findIndex(
+				method => method.authProvider === provider
+			);
+
+			if (authIndex > -1) {
+				let authMethod = user.authMethods[authIndex];
+				await updateAuthMethod(authMethod, callbackData);
+			} else {
+				let newAuthMethod = await createAuthMethod(callbackData);
+
+				user.authMethods.push(newAuthMethod);
+			}
+		}
+
+		await user.save();
+
+		return user;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
+
+async function createAuthMethod(callbackData) {
+	try {
+		const {
+			accessToken,
+			refreshToken,
+			expires_in,
+			profile: { provider, id, profileUrl },
+		} = callbackData;
+
+		let newAuthMethod = await Auth.create({
+			authProvider: provider,
+			profileId: id,
+			encryptedAuthToken: accessToken,
+			encryptedRefreshToken: refreshToken,
+			expirationDate: new Date(Date.now() + expires_in * 1000),
+			profileLink: profileUrl,
+		});
+		return newAuthMethod;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
+
+async function updateAuthMethod(authMethod, callbackData) {
+	try {
+		const { accessToken, refreshToken, expires_in } = callbackData;
+		authMethod.encryptedAuthToken = accessToken;
+		authMethod.encryptedRefreshToken = refreshToken;
+		authMethod.expirationDate = new Date(Date.now() + expires_in * 1000);
+		await authMethod.save();
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
