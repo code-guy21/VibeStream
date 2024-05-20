dae986e3d26aa1b84c693884cf26b3f1b35d971b
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-05-20 11:01:14 -0400

    added functional search bar on front-end to pull tracks, display them, and enable playback

--- client/src/containers/Playback/Playback.jsx
+++ client/src/containers/Playback/Playback.jsx
@@ -1,10 +1,52 @@
+import { useState, useEffect } from 'react';
 import PlayBackPage from '../../pages/PlaybackPage';
 
 const PlayBack = () => {
+	const [searchTerm, setSearchTerm] = useState('');
+	const [results, setResults] = useState([]);
+	const [track, setTrack] = useState({});
+	const [token, setToken] = useState('');
+	const [isPaused, setPaused] = useState(false);
+	const [isActive, setActive] = useState(false);
+	const [player, setPlayer] = useState(null);
+
+	async function searchTracks() {
+		try {
+			const resp = await fetch(
+				`/api/spotify/search?term=${searchTerm}&type=track`
+			);
+			const data = await resp.json();
+
+			console.log(data.tracks.items);
+
+			setResults(data.tracks.items);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		let storedToken = localStorage.getItem('access_token');
+
+		setToken(storedToken);
+	});
+
 	return (
-		<div>
-			<PlayBackPage></PlayBackPage>
-		</div>
+		<>
+			<PlayBackPage
+				setPaused={setPaused}
+				setActive={setActive}
+				setPlayer={setPlayer}
+				isPaused={isPaused}
+				isActive={isActive}
+				player={player}
+				setSearchTerm={setSearchTerm}
+				searchTracks={searchTracks}
+				results={results}
+				setTrack={setTrack}
+				track={track}
+				token={token}></PlayBackPage>
+		</>
 	);
 };
 
--- client/src/pages/PlaybackPage/PlaybackPage.jsx
+++ client/src/pages/PlaybackPage/PlaybackPage.jsx
@@ -1,5 +1,69 @@
-const PlayBackPage = () => {
-	return <>Playback</>;
+import { Input, Label, Field, Fieldset, Button } from '@headlessui/react';
+import PlaybackControl from '../../components/PlaybackControl';
+
+const PlayBackPage = ({
+	setSearchTerm,
+	searchTracks,
+	results,
+	setTrack,
+	track,
+	token,
+	setActive,
+	setPaused,
+	setPlayer,
+	isPaused,
+	isActive,
+	player,
+}) => {
+	return (
+		<div className='flex flex-col py-10 px-6 min-h-full'>
+			<Fieldset>
+				<Field className='flex justify-center'>
+					<Input
+						className='rounded-md flex-1'
+						placeholder='Search songs, albums, artists'
+						onChange={e => setSearchTerm(e.target.value)}
+						type='text'></Input>
+					<Button
+						className='flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+						onClick={searchTracks}>
+						submit
+					</Button>
+				</Field>
+			</Fieldset>
+			<div className='flex flex-col mt-5'>
+				{results?.map((t, i) => {
+					return (
+						<div className='flex w-full h-16 m-1  border-2'>
+							<Button onClick={() => setTrack(t)} key={i}>
+								<img className='w-16 h-full' src={t.album.images[0].url}></img>
+							</Button>
+							<div className='flex flex-1 flex-col px-2'>
+								<div className='text-md font-medium'>{t.name}</div>
+								<div className='text-sm'>
+									{t.artists
+										.map(art => {
+											return art.name;
+										})
+										.join(', ')}
+								</div>
+							</div>
+						</div>
+					);
+				})}
+				<PlaybackControl
+					setActive={setActive}
+					setPaused={setPaused}
+					setPlayer={setPlayer}
+					isPaused={isPaused}
+					isActive={isActive}
+					player={player}
+					track={track}
+					token={token}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default PlayBackPage;
