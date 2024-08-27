eda4db55da03907d7edb87c36ba2550eb16eb6a8
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-08-27 13:36:48 -0400

    added full screen option to visualizer component

--- client/src/pages/VisualizationPage/VisualizationPage.jsx
+++ client/src/pages/VisualizationPage/VisualizationPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import VisualizationSwitcher from '../../components/VisualizationSwitcher';
 import WaveformVisualization from '../../components/WaveFormVisualization';
 import CrystalOrbVisualization from '../../components/CrystalOrbVisualization';
@@ -6,12 +6,44 @@ import styles from './VisualizationPage.module.css';
 
 const VisualizationPage = () => {
 	const [currentVisualization, setVisualization] = useState('crystal');
+	const [isFullScreen, setIsFullScreen] = useState(false);
+	const visualizationRef = useRef(null);
+
+	const toggleFullScreen = () => {
+		if (!document.fullscreenElement) {
+			visualizationRef.current.requestFullscreen().catch(err => {
+				console.error(
+					`Error attempting to enable full-screen mode: ${err.message}`
+				);
+			});
+		} else {
+			document.exitFullscreen();
+		}
+	};
+
+	useEffect(() => {
+		const handleFullScreenChange = () => {
+			setIsFullScreen(!!document.fullscreenElement);
+		};
+
+		document.addEventListener('fullscreenchange', handleFullScreenChange);
+
+		return () => {
+			document.removeEventListener('fullscreenchange', handleFullScreenChange);
+		};
+	}, []);
 
 	return (
-		<div className={styles.visualizationPage}>
+		<div
+			className={`${styles.visualizationPage} ${
+				isFullScreen ? styles.fullScreen : ''
+			}`}
+			ref={visualizationRef}>
 			<VisualizationSwitcher
 				currentVisualization={currentVisualization}
 				setVisualization={setVisualization}
+				isFullScreen={isFullScreen}
+				toggleFullScreen={toggleFullScreen}
 			/>
 			<div className={styles.visualizationContainer}>
 				{currentVisualization === 'crystal' && <CrystalOrbVisualization />}
