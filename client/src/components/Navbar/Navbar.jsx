f6c34e2a9f04cbab0531777e66e32c9b147294d3
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-08-27 13:59:27 -0400

    updated default profile picture

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/images/vibestream-logo.svg';
+import defaultProfileIcon from '../../assets/images/default-profile-icon.svg';
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ');
@@ -161,11 +162,19 @@ export default function Navbar() {
 											<Menu.Button className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
 												<span className='absolute -inset-1.5' />
 												<span className='sr-only'>Open user menu</span>
-												<img
-													className='h-8 w-8 rounded-full'
-													src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-													alt=''
-												/>
+												{state.user && state.user.profileImage ? (
+													<img
+														className='h-8 w-8 rounded-full'
+														src={state.user.profileImage}
+														alt=''
+													/>
+												) : (
+													<img
+														className='h-8 w-8 rounded-full'
+														src={defaultProfileIcon}
+														alt=''
+													/>
+												)}
 											</Menu.Button>
 										</div>
 										<Transition
